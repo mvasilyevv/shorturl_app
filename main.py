97fef9d8b901617f214804f96e2066fcb30b22c0
markvasilev <mark.vasilyevv@gmail.com>
2025-01-03 14:04:53 +0200

    Added docker-compose.yml and new endpoints

--- main.py
+++ main.py
@@ -36,8 +36,21 @@ def shorten_url(item: URLCreate, db: Session = Depends(get_db)):
             db.add(new_item)
             db.commit()
             db.refresh(new_item)
-            return {"short_url": f"http://localhost:8000/{short_id}"}
+            return {
+                "short_url": f"http://localhost:8081/{short_id}",
+                "short_id": f"{short_id}"
+            }
     raise HTTPException(status_code=500, detail="Не удалось сгенерировать короткую ссылку")
+
+@app.get("/all_urls")
+def get_all_urls(db: Session = Depends(get_db)):
+    url_items = db.query(URLItem).all()
+    return [
+        {
+            "short_id": item.short_id,
+            "full_url": item.full_url
+        } for item in url_items
+    ]
 
 @app.get("/{short_id}")
 def redirect_to_full(short_id: str, db: Session = Depends(get_db)):
@@ -56,5 +69,11 @@ def get_stats(short_id: str, db: Session = Depends(get_db)):
         "full_url": url_item.full_url
     }
 
-#  Все сгенерированные ссылки
-# Удалить сгенерированную ссылку
+@app.delete("/delete/{short_id}")
+def delete_short_url(short_id: str, db: Session = Depends(get_db)):
+    url_item = db.query(URLItem).filter(URLItem.short_id == short_id).first()
+    if not url_item:
+        raise HTTPException(status_code=404, detail="Короткая ссылка не найдена")
+    db.delete(url_item)
+    db.commit()
+    return {"detail": f"Короткая ссылка '{short_id}' успешно удалена"}
